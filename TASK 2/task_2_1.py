de833ae1f19f2b85108639444207b664b0a86d97
Big_Iron01 <mateusz.kostyra@gmail.com>
2022-12-23 17:27:55 +0100

    Task 2

--- TASK 2/task_2_1.py
+++ TASK 2/task_2_1.py
@@ -1,1 +1,14 @@
-print("test")
+rpc = {"rock": 1, "paper": 2, "scissors": 3}
+elf = {"A": "rock", "B": "paper", "C": "scissors"}
+man = {"X": "rock", "Y": "paper", "Z": "scissors"}
+
+with open("text_2.txt", "r") as file:
+    games = file.read().split("\n")
+
+tally = 0
+for _ in games:
+    if _:
+        current = ((((rpc[man[_[2]]]) - 1 - rpc[elf[_[0]]] - 1) % 3) * 3 + rpc[man[_[2]]])
+        tally += current
+
+print(tally)  # 11666
